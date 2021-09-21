6329eda65359aa184fc86e1449e5e3cac1a4cf20
Raymond <raymond@philippinedev.com>
2021-09-21 14:18:35 +0800

    enabled create button

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,4 +8,21 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.new
   end
 
+  def create
+    @transaction = Transaction.new(trans_params)
+    if @transaction.save
+      redirect_to transactions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trans_params
+    params
+      .require(:transaction)
+      .permit(:name, :description, :budget, :cut_off, :due_date, :payment)
+  end
+
 end
