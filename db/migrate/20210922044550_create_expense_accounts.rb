552e6f80b7261e5af3121ec839a1bc8ea650cc3d
Abbie <abbie@philippinedev.com>
2021-09-22 14:22:31 +0800

    CRUD for Accounts

--- db/migrate/20210922044550_create_expense_accounts.rb
+++ db/migrate/20210922044550_create_expense_accounts.rb
@@ -3,6 +3,7 @@ class CreateExpenseAccounts < ActiveRecord::Migration[6.1]
     create_table :expense_accounts do |t|
       t.string :name, null: false, index: { unique: true }
       t.string :description
+      t.string :category
 
       t.timestamps
     end
