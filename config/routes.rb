655fa4875dfbe4db95f1aef2a1d1a15df98fbd59
Raymond <raymond@philippinedev.com>
2021-09-23 20:51:27 +0800

    added controller transaction_types

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :dashboards
   resources :transactions
   resources :accounts
-  resources :dashboards
+  resources :transaction_types
 
   root to: 'dashboards#index'
 end
