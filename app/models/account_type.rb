1432880e54d2c7919c76e6c9ef85b93416250513
Raymond <raymond@morphos.is>
2021-09-29 01:05:02 +0800

    Added back transaction update and destroy for un-actualized only

--- app/models/account_type.rb
+++ app/models/account_type.rb
@@ -1,9 +1,9 @@
 class AccountType < ApplicationRecord
-  BANK_ACCOUNT             = "Bank Account".freeze
-  CREDIT_CARD              = "Credit Card".freeze
-  CASH                     = "Cash".freeze
-  EMPLOYER                 = "Employer".freeze
-  EMPLOYEE                 = "Employee".freeze
+  BANK_ACCOUNT = "Bank Account".freeze
+  CREDIT_CARD  = "Credit Card".freeze
+  CASH         = "Cash".freeze
+  EMPLOYER     = "Employer".freeze
+  EMPLOYEE     = "Employee".freeze
 
   has_many :accounts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_023626) do
     t.string "amount_currency", default: "PHP", null: false
     t.date "cutoff_date"
     t.date "due_date"
-    t.date "actualized_on"
+    t.datetime "actualized_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["source_account_id"], name: "index_transactions_on_source_account_id"
