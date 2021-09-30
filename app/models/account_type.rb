a8935e6a69a822b4137906e4c549040d728d4c03
Raymond <raymond@philippinedev.com>
2021-09-30 18:51:11 +0800

    deleted old models

--- app/models/account_type.rb
+++
@@ -1,9 +0,0 @@
-class AccountType < ApplicationRecord
-  BANK_ACCOUNT = "Bank Account".freeze
-  CREDIT_CARD  = "Credit Card".freeze
-  CASH         = "Cash".freeze
-  EMPLOYER     = "Employer".freeze
-  EMPLOYEE     = "Employee".freeze
-
-  has_many :accounts
-end
