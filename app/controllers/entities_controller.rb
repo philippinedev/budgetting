7973363186e286425e2c971d8285b7bef8dc4a2c
Raymond <raymond@morphos.is>
2021-09-30 10:26:50 +0800

    bugfix: saving of parent entity

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -4,8 +4,7 @@ class EntitiesController < ApplicationController
   end
 
   def new
-    @parent = Entity.find(params[:parent_id])
-    @entity = @parent.entities.new
+    set_parent_and_entity
   end
 
   def show
@@ -13,8 +12,7 @@ class EntitiesController < ApplicationController
   end
 
   def create
-    @parent = Entity.find(params[:entity][:parent_id])
-    @entity = @parent.entities.new(entity_params)
+    set_parent_and_entity(create: true)
 
     if @entity.save
       redirect_to entities_path, notice: "Entity was successfully created."
@@ -34,6 +32,30 @@ class EntitiesController < ApplicationController
   end
 
   private
+
+  def set_parent_and_entity(create: false)
+    if params[:parent_id].present?
+      @parent = Entity.find(params[:parent_id])
+    elsif params[:entity] && params[:entity][:parent_id].present?
+      @parent = Entity.find(params[:entity][:parent_id])
+    else
+      @parent = nil
+    end
+
+    if create
+      if @parent.present?
+        @entity = @parent.entities.new(entity_params)
+      else
+        @entity = Entity.new(entity_params)
+      end
+    else
+      if @parent.present?
+        @entity = @parent.entities.new
+      else
+        @entity = Entity.new
+      end
+    end
+  end
 
   def entity_params
     params.require(:entity)
