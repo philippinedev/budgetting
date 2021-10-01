fa91da828e89bbdc31cf1180ec1ed644ea5e49c4
Raymond <raymond@philippinedev.com>
2021-10-01 17:35:15 +0800

    remove income and salary expense

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
 
   resources :dashboards
   resources :transactions
-  resources :incomes
-  resources :salary_expenses
   resources :accounts do
     member do
       patch 'init'
@@ -16,7 +14,6 @@ Rails.application.routes.draw do
     end
   end
   resources :transaction_types
-  resources :account_types
   resources :summaries, only: [:index, :show]
   resources :entities
 end
