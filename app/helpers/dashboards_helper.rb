a447041e420bb479fd86f1dbf13333d9ad9dc6f4
Raymond <raymond@philippinedev.com>
2021-10-03 00:12:03 +0800

    remove hilite of parents

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,11 +1,14 @@
 module DashboardsHelper
   def hilite(value_hash)
     return '' unless value_hash[:updated]
+    return '' unless value_hash[:is_account]
 
-    if value_hash[:is_account]
-      value_hash[:increased_by] < 0 ? 'hilite-danger-primary' : 'hilite-primary'
-    else
-      value_hash[:increased_by] < 0 ? 'hilite-danger' : 'hilite'
-    end
+    # if value_hash[:is_account]
+    #   value_hash[:increased_by] < 0 ? 'hilite-danger-primary' : 'hilite-primary'
+    # else
+    #   value_hash[:increased_by] < 0 ? 'hilite-danger' : 'hilite'
+    # end
+
+    "hilite #{(value_hash[:increased_by] < 0 ? 'hilite-danger' : '')} hilite-bold"
   end
 end
