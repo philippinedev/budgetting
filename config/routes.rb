fa6b434fd0d16face536460b20d16eb0aefdb86e
Don <forrest@philippinedev.com>
2021-10-05 13:23:07 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '*path', to: 'application#routing_error'
-
   root to: 'dashboards#index'
 
   devise_for :users
@@ -21,4 +19,5 @@ Rails.application.routes.draw do
 
   # resources :summaries, only: [:index, :show]
   # get 'summaries/index'
+  get '*path', to: 'application#routing_error'
 end
