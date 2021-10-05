5786764365b1750c376b80964f3b31124af22cee
Don <forrest@philippinedev.com>
2021-10-05 15:02:18 +0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   end
 
   def routing_error
-  render :file => "#{Rails.root}/public/404.html",  :status => 404
+    render file: "#{Rails.root}/public/404.html", status: 404
   end
 end
