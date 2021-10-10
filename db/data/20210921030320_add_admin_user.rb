4f85c55af04de881d2067f4d56ff28bcde2d24a8
Raymond <raymond@philippinedev.com>
2021-10-10 21:36:05 +0800

    convert username to first and last name

--- db/data/20210921030320_add_admin_user.rb
+++ db/data/20210921030320_add_admin_user.rb
@@ -4,7 +4,8 @@ class AddAdminUser < ActiveRecord::Migration[6.1]
   def up
     user = User.new
     user.admin = true
-    user.username = 'admin'
+    user.first_name = 'admin'
+    user.last_name = 'two'
     user.email = 'admin@test.com'
     user.password = 'password'
     user.password_confirmation = 'password'
